20c5bc2430b42ee5045850b4e6a128325e0622b3
Aaryan <sonofnaleen@gmail.com>
2025-05-11 14:14:14 +0545

    ETL completed

--- etl.py
+++ etl.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import sqlalchemy
+
 
 Base_Url="http://universities.hipolabs.com/search?country=Canada"
 def extract()->dict:
@@ -14,11 +16,27 @@ def transform(data:dict)->pd.DataFrame:
     df=df.reset_index(drop=True)
     return df[["name","domains","web_pages"]]
        
-       
+def load(df:pd.DataFrame):
+    
+    try:
+        engine = sqlalchemy.create_engine(
+            "postgresql+psycopg2://Aaryan:ads@localhost:5432/university"
+        )
+        print("Rows in transformed_data:", len(transformed_data))
+        print(transformed_data.head())
+
+        df.to_sql('ontario',engine,if_exists='replace',index=False)
+        with engine.connect() as conn:
+            result = conn.execute(sqlalchemy.text("SELECT COUNT(*) FROM Ontario"))
+            count = result.scalar_one()   # scalar_one() returns the single value
+            print(f"Rows now in database: {count}")
+    except Exception as e:
+        print(e)
+
     
 
 
 data=extract()
 transformed_data=transform(data)
-print(transformed_data)
+load(transformed_data)
 
