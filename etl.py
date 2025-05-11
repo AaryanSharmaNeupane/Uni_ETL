6a0e645e30389590c6a484dbf32e8e3c4581a768
Aaryan <sonofnaleen@gmail.com>
2025-05-11 08:51:19 +0545

    transformation

--- etl.py
+++ etl.py
@@ -1,9 +1,24 @@
 import requests
+import pandas as pd
 
 Base_Url="http://universities.hipolabs.com/search?country=Canada"
 def extract()->dict:
     result=requests.get(Base_Url).json()
     return result
 
+def transform(data:dict)->pd.DataFrame:
+    df=pd.DataFrame(data)
+    df=df[df["state-province"].str.contains('Ontario',na=False)]
+    df['domains']=[",".join(map(str,d)) for d in df["domains"]]
+    df['web_pages']=[",".join(map(str,d)) for d in df["web_pages"]]
+    df=df.reset_index(drop=True)
+    return df[["name","domains","web_pages"]]
+       
+       
+    
+
+
 data=extract()
-print(data)
+transformed_data=transform(data)
+print(transformed_data)
+
