503d94d86be7f72ef5d426b8a0e3e956926a4bba
Aaryan <sonofnaleen@gmail.com>
2025-05-11 08:21:36 +0545

    extraction

--- etl.py
+++ etl.py
@@ -1,1 +1,9 @@
-print("hello")
+import requests
+
+Base_Url="http://universities.hipolabs.com/search?country=Canada"
+def extract()->dict:
+    result=requests.get(Base_Url).json()
+    return result
+
+data=extract()
+print(data)
